156ed5ef32a5ab636a8af1f2724d1f674ee30c17
theqwertycoder <theqwertycoder@gmail.com>
2024-06-20 19:49:07 -0400

    updated layer to include most of the builtin objects, modified test code to include input

--- layer.cpp
+++ layer.cpp
@@ -19,11 +19,24 @@ openmini. If not, see <https://www.gnu.org/licenses/>.
 *******************************************************************************/
 #include "api/everything.hpp"
 #include "api/main.hpp"
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
 #include <SDL2pp/Renderer.hh>
 #include <SDL2pp/SDL2pp.hh>
 #include <SDL2pp/Texture.hh>
 #include <SDL2pp/Window.hh>
 #include <cstdint>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <system_error>
+#include <vector>
 using namespace SDL2pp;
 openmini::screen::screen() {}
 void openmini::screen::draw(uint16_t *buf, uint16_t x, uint16_t y, uint8_t width, uint8_t height) {}
@@ -65,4 +78,133 @@ struct screen : openmini::screen {
 	}
 };
 screen builtin_screen;
+
+int openmini::bus::uart::send(void *buf, int length) {
+	std::cerr.write(static_cast<const char*>(buf),length);
+	return length;
+}
+int openmini::bus::uart::recv(void *buf, int length) {
+	std::cin.read(static_cast<char*>(buf),length);
+	return length;
+}
+struct sdl2input : openmini::input {
+	bool poll() override {
+		for (int i = 0; i < 16; i++) changed[i]=false;
+		SDL_Event event;
+		bool flag = false;
+		while (SDL_PollEvent(&event)) {
+			if (event.type==SDL_QUIT) {
+				std::exit(0);
+			} else if ((event.type==SDL_KEYDOWN)||(event.type==SDL_KEYUP)) {
+				bool ka = event.type==SDL_KEYDOWN;
+				switch (event.key.keysym.sym) {
+					case SDLK_RETURN: // OK
+						active[button::ok]=ka;
+						changed[button::ok]=true;
+						flag=true;
+						break;
+					case SDLK_UP: // up
+						active[button::up]=ka;
+						changed[button::up]=true;
+						flag=true;
+						break;
+					case SDLK_DOWN: // down
+						active[button::down]=ka;
+						changed[button::down]=true;
+						flag=true;
+						break;
+					case SDLK_LEFT: // left
+						active[button::left]=ka;
+						changed[button::left]=true;
+						flag=true;
+						break;
+					case SDLK_RIGHT: // right
+						active[button::right]=ka;
+						changed[button::right]=true;
+						flag=true;
+						break;
+					case SDLK_BACKSPACE: // back
+						active[button::back]=ka;
+						changed[button::back]=true;
+						flag=true;
+						break;
+					case SDLK_h: // home
+						active[button::home]=ka;
+						changed[button::home]=true;
+						flag=true;
+						break;
+					case SDLK_m: // menu
+						active[button::menu]=ka;
+						changed[button::menu]=true;
+						flag=true;
+						break;
+					case SDLK_ESCAPE: // quit
+						active[button::quit]=ka;
+						changed[button::quit]=true;
+						flag=true;
+						break;
+					case SDLK_1: // f1
+						active[button::f1]=ka;
+						changed[button::f1]=true;
+						flag=true;
+						break;
+					case SDLK_2: // f2
+						active[button::f2]=ka;
+						changed[button::f2]=true;
+						flag=true;
+						break;
+					case SDLK_3: // f3
+						active[button::f3]=ka;
+						changed[button::f3]=true;
+						flag=true;
+						break;
+				}
+			}
+		}
+		return flag;
+	}
+};
+struct builtinfs : openmini::storage::filesystem {
+	struct file : openmini::storage::file {
+		const bool readonly;
+		std::fstream f;
+		std::string name;
+		file(std::string name, bool readonly) : name(name),readonly(readonly),f(name,std::ios_base::in|std::ios_base::openmode(readonly?0:std::ios_base::out)|std::ios_base::binary) {}
+		void write(uint64_t pos, void *buf, uint32_t size) override {
+			f.seekp(pos,f.beg);
+			f.write(static_cast<const char*>(buf),size);
+		}
+		void read(uint64_t pos, void *buf, uint32_t size) override {
+			f.seekg(pos,f.beg);
+			f.read(static_cast<char*>(buf),size);
+		}
+		void resize(uint64_t size) override {
+			f.close();
+			std::error_code ec;
+			std::filesystem::resize_file(std::filesystem::path(name), size, ec);
+			f=std::fstream(name,std::ios_base::in|std::ios_base::openmode(readonly?0:std::ios_base::out)|std::ios_base::binary);
+		}
+		void close() override {
+			f.close();
+		}
+	};
+	std::unique_ptr<openmini::storage::file> open(std::string path, bool ro = true) override {
+		file *f = new file{path,ro};
+		return std::unique_ptr<openmini::storage::file>(static_cast<openmini::storage::file*>(f));
+	}
+	std::vector<std::string> ls(std::string path) override {
+		std::vector<std::string> files;
+		auto iter = std::filesystem::directory_iterator(path);
+		for (auto x : iter)
+			files.emplace_back(x.path().filename().string());
+		return files;
+	}
+};
+sdl2input builtin_input;
+openmini::bus::pin nil_pin;
+openmini::bus::uart builtin_debug_uart{nil_pin,nil_pin};
 openmini::screen &openmini::builtin::screen = builtin_screen;
+openmini::bus::uart &openmini::builtin::debug = builtin_debug_uart;
+openmini::input &openmini::builtin::input = builtin_input;
+builtinfs builtin_fs;
+openmini::storage::filesystem &openmini::builtin::fs = builtin_fs;
--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include "api/main.hpp"
 #include "api/screen.hpp"
 #include "api/builtin.hpp"
+#include "api/input.hpp"
 #include <chrono>
 #include <thread>
 
@@ -15,8 +16,18 @@ int main(void) {
 		0x8000,0xffff,0xffff,0xffff,0xffff,0xffff,0xffff,0x8000,
 		0x8000,0x8000,0x8000,0x8000,0x8000,0x8000,0x8000,0x8000
 	};
-	openmini::builtin::screen.draw(testimg, 0, 0, 8, 8);
-	openmini::builtin::screen.sync();
-	std::this_thread::sleep_for(std::chrono::seconds(5));
+	int x = 0, y = 0;
+	while (true) {
+		openmini::builtin::screen.draw(testimg, x, y, 8, 8);
+		openmini::builtin::screen.sync();
+		openmini::builtin::input.poll();
+		if (openmini::builtin::input.active[openmini::input::button::up]) y--;
+		if (openmini::builtin::input.active[openmini::input::button::down]) y++;
+		if (openmini::builtin::input.active[openmini::input::button::left]) x--;
+		if (openmini::builtin::input.active[openmini::input::button::right]) x++;
+		if (openmini::builtin::input.active[openmini::input::button::quit]) break;
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+	std::this_thread::sleep_for(std::chrono::seconds(3));
 	return 0;
 }
